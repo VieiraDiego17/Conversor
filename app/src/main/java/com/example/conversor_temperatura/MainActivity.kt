65a14474839d3e4a3b94cd6eed557780e035540f
Diego Vieira <diegovieira.ads@gmail.com>
2022-12-06 19:06:12 -0300

    Alterando o uso de if para usar when

--- app/src/main/java/com/example/conversor_temperatura/MainActivity.kt
+++ app/src/main/java/com/example/conversor_temperatura/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.conversor_temperatura
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.conversor_temperatura.databinding.ActivityMainBinding
 import java.util.zip.Inflater
 
@@ -19,7 +20,7 @@ class MainActivity : AppCompatActivity() {
             converter()
         }
     }
-
+/*
     fun converter(){
         val temperatura = binding.textTemperatura.text.toString().toDouble()
 
@@ -52,5 +53,39 @@ class MainActivity : AppCompatActivity() {
             binding.textResultado.setText("%.2f".format(resultado))
         }
     }
+    */
+        fun converter(){
+        val temperatura = binding.textTemperatura.text.toString().toDouble()
+        val cF = (temperatura * 1.8) + 32
+        val cK = temperatura + 273
+        val cC = temperatura
+        val fC = (temperatura -32) / 1.8
+        val fK = (temperatura + 459.67) * 5 / 9
+        val fF = temperatura
+        val kC = temperatura - 273
+        val kF = ((temperatura -273) *9/5) + 32
+        val kK = temperatura
+
+        when{
+            binding.radioCelciusPrimeiro.isChecked && binding.radioFahrenheitSegundo.isChecked ->
+                binding.textResultado.setText("%.2f".format(cF))
+            binding.radioCelciusPrimeiro.isChecked && binding.radioKelvinSegundo.isChecked ->
+                binding.textResultado.setText("%.2f".format(cK))
+            binding.radioCelciusPrimeiro.isChecked && binding.radioCelciusSegundo.isChecked ->
+                binding.textResultado.setText("%.2f".format(cC))
+            binding.radioFahrenheitPrimeiro.isChecked && binding.radioCelciusSegundo.isChecked ->
+                binding.textResultado.setText("%.2f".format(fC))
+            binding.radioFahrenheitPrimeiro.isChecked && binding.radioKelvinSegundo.isChecked ->
+                binding.textResultado.setText("%.2f".format(fK))
+            binding.radioFahrenheitPrimeiro.isChecked && binding.radioFahrenheitSegundo.isChecked ->
+                binding.textResultado.setText("%.2f".format(fF))
+            binding.radioKelvinPrimeiro.isChecked && binding.radioCelciusSegundo.isChecked ->
+                binding.textResultado.setText("%.2f".format(kC))
+            binding.radioKelvinPrimeiro.isChecked && binding.radioFahrenheitSegundo.isChecked ->
+                binding.textResultado.setText("%.2f".format(kF))
+            binding.radioKelvinPrimeiro.isChecked && binding.radioKelvinSegundo.isChecked ->
+                binding.textResultado.setText("%.2f".format(kK))
+        }
+        }
 }
 
