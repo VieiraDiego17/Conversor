82a221798d4bdf017c969aef3c7ca2e044e88fac
Diego Vieira <diegovieira.ads@gmail.com>
2022-12-10 14:38:19 -0300

    Alterando as cores

--- app/src/main/java/com/example/conversor_temperatura/MainActivity.kt
+++ app/src/main/java/com/example/conversor_temperatura/MainActivity.kt
@@ -12,9 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar!!.hide()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
 
         usarBinding()
 
@@ -22,26 +19,26 @@ class MainActivity : AppCompatActivity() {
             converter()
         }
     }
- 
-        fun usarBinding(){
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            val view = binding.root
-            setContentView(view)
-        }
 
-        fun converter(){
+    fun usarBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun converter() {
         val temperatura = binding.textTemperatura.text.toString().toDouble()
         val cF = (temperatura * 1.8) + 32
         val cK = temperatura + 273
         val cC = temperatura
-        val fC = (temperatura -32) / 1.8
+        val fC = (temperatura - 32) / 1.8
         val fK = (temperatura + 459.67) * 5 / 9
         val fF = temperatura
         val kC = temperatura - 273
-        val kF = ((temperatura -273) *9/5) + 32
+        val kF = ((temperatura - 273) * 9 / 5) + 32
         val kK = temperatura
 
-        when{
+        when {
             binding.radioCelciusPrimeiro.isChecked && binding.radioFahrenheitSegundo.isChecked ->
                 binding.textResultado.setText("%.2f".format(cF))
             binding.radioCelciusPrimeiro.isChecked && binding.radioKelvinSegundo.isChecked ->
@@ -61,6 +58,6 @@ class MainActivity : AppCompatActivity() {
             binding.radioKelvinPrimeiro.isChecked && binding.radioKelvinSegundo.isChecked ->
                 binding.textResultado.setText("%.2f".format(kK))
         }
-        }
+    }
 }
 
